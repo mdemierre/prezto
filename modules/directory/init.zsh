2c60cbcea3f298537ae574174c5342dc57897045
Marc Demierre <marc.demierre@gmail.com>
2017-06-10 13:43:08 +0200

    Add my config and prompt

--- modules/directory/init.zsh
+++ modules/directory/init.zsh
@@ -18,7 +18,8 @@ setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
 setopt CDABLE_VARS          # Change directory to a path stored in a variable.
 setopt MULTIOS              # Write to multiple descriptors.
 setopt EXTENDED_GLOB        # Use extended globbing syntax.
-unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
+# Note: disabled because it causes issues with JetBrains IDEs terminal emulators.
+#unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
                             # Use >! and >>! to bypass.
 
 #
